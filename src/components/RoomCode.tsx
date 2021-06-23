d4eb99c758910e83670bf8ec6045dc96bec8aa1f
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-06-23 18:21:26 -0300

    feat: Create Room page view

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import copyImg from 'assets/images/copy.svg'
+
+import 'styles/room-code.scss'
+
+type RoomCodeProps = {
+    roomCode: string
+}
+
+export const RoomCode = ({ roomCode }: RoomCodeProps) => {
+
+    const handleCopyToClipboard = React.useCallback(() => {
+        navigator.clipboard.writeText(roomCode)
+    }, [roomCode])
+
+    return (
+        <button className="room-code" onClick={handleCopyToClipboard}>
+            <img src={copyImg} alt="Copy room code" />
+            <span>Sala #{roomCode}</span>
+        </button>
+    )
+}
+
+export default RoomCode
--- src/pages/Room/Room.tsx
+++ src/pages/Room/Room.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
+import logoImg from 'assets/images/logo.svg'
+import { Button } from 'components/Button'
+import RoomCode from 'components/RoomCode'
+
+import 'styles/room.scss'
+
 type RoomParams = {
     id: string
 }
@@ -9,8 +15,31 @@ export const Room = () => {
     const { id } = useParams<RoomParams>()
 
     return (
-        <div>
-            Room - {id}
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <RoomCode roomCode={id} />
+                </div>
+            </header>
+
+            <main>
+                <div className="room-title">
+                    <h1>Sala React</h1>
+                    <span>4 perguntas</span>
+                </div>
+
+                <form>
+                    <textarea 
+                        placeholder="O que vc quer perguntar?"
+                    />
+
+                    <div className="form-footer">
+                        <span>Para enviar uma pergunta, <button>faça seu login</button></span>
+                        <Button type="submit">Enviar pergunta</Button>
+                    </div>
+                </form>
+            </main>
         </div>
     )
 }
